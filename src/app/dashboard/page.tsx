01ff9230e948285ef0b7bf2f862717aef27af579
Amir Mohammad Fallah <amirmfallah@yahoo.com>
2024-04-28 19:52:09 +1000

    added openai integration

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useSession } from "next-auth/react";
+import { useChat } from "ai/react";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
@@ -9,18 +13,48 @@ import {
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { CornerDownLeft, Mic, Paperclip } from "lucide-react";
+import { auth } from "@/auth";
 
 export default function Page() {
+  const session = useSession();
+
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
   return (
     <div className="relative flex h-full min-h-[50vh] flex-col rounded-xl bg-muted/50 p-4 lg:col-span-2">
       <TooltipProvider>
-        <Badge variant="outline" className="absolute right-3 top-3">
-          Output
-        </Badge>
-        <div className="flex-1" />
+        <div className="space-y-6 max-h-[70vh] overflow-auto flex-1 pb-6 flex flex-col">
+          {messages.map((m) => (
+            // <div key={m.id} className="whitespace-pre-wrap">
+            //   {m.role === "user" ? "User: " : "AI: "}
+
+            // </div>
+            <div
+              key={m.id}
+              className={
+                "flex flex-col w-full max-w-[320px] rounded-xl leading-1.5 p-4 border-gray-200 bg-gray-200 dark:bg-gray-700 " +
+                (m.role === "user"
+                  ? "ml-auto rounded-br-none"
+                  : "rounded-bl-none")
+              }
+            >
+              <div className="flex items-center space-x-2 rtl:space-x-reverse">
+                <span className="text-sm font-semibold text-gray-900 dark:text-white">
+                  {m.role === "user" ? session.data?.user?.name : "Chatbot"}
+                </span>
+                <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
+                  11:46
+                </span>
+              </div>
+              <p className="text-sm font-normal py-2.5 text-gray-900 dark:text-white">
+                {m.content}
+              </p>
+            </div>
+          ))}
+        </div>
         <form
           className="relative overflow-hidden rounded-lg border bg-background focus-within:ring-1 focus-within:ring-ring"
           x-chunk="dashboard-03-chunk-1"
+          onSubmit={handleSubmit}
         >
           <Label htmlFor="message" className="sr-only">
             Message
@@ -29,6 +63,8 @@ export default function Page() {
             id="message"
             placeholder="Type your message here..."
             className="min-h-12 resize-none border-0 p-3 shadow-none focus-visible:ring-0"
+            value={input}
+            onChange={handleInputChange}
           />
           <div className="flex items-center p-3 pt-0">
             <Tooltip>
